8e594f5cc5ce3df674a896c210a7219e2eecca66
Christopher-Masselli <christophermasselli@gmail.com>
2019-04-04 21:59:44 -0400

    v2 just need to add cmdline pass through instead of hardcode inputs

--- pyGroupDetect.py
+++ pyGroupDetect.py
@@ -1,110 +1,73 @@
 #Written by Christopher Masselli
 #christophermasselli@gmail.com
 
-from PIL import Image
+
 import pytesseract
 import argparse
 import cv2
-import os
 import xlsxwriter
 import re
-import numpy
-class Group:
-    def __init__(self, groupname, data):
-        self.groupname = groupname
-        self.data = data
-    
+
+#Creates an array of images corresponding to groups
 def crop_Image(img): 
     h, w = img.shape[:2]
-    hoffset = 70
-    woffset =  1140
-    x = 0
+    hoffset = 38
+    woffset =  2317
+    x = 60
     leftsides = []
-    for y in range (0, h, hoffset):
+    rightsides =[]
+    for y in range (48, 199, hoffset):
             y1 = y + hoffset
             x1 = x + woffset
             tiles = img[y:y+hoffset,x:x+woffset]
             leftsides.append(tiles)
-            cv2.rectangle(img, (x,y), (x1,y1), (0,255,0))
-            cv2.imwrite("save/" + str(x) +' _' + str(y) + ".png", tiles)
-            cv2.imwrite("test.png", img)
-    x = 1140 + 80
-    rightsides = []
-    for y in range (0, h, hoffset):
+            #under used for debug
+            #cv2.rectangle(img, (x,y), (x1,y1), (0,255,0))
+           # cv2.imwrite("save/" + str(x) +' _' + str(y) + ".png", tiles)
+            #cv2.imwrite("test.png", img)
+
+    for y in range (612, 615+123, hoffset):
             y1 = y + hoffset
             x1 = x + woffset
             tiles = img[y:y+hoffset,x:x+woffset]
             rightsides.append(tiles)
-            cv2.rectangle(img, (x,y), (x1,y1), (0,255,0))
-            cv2.imwrite("save/" + str(x) +' _' + str(y) + ".png", tiles)
-            cv2.imwrite("test.png", img)
+            #under used for debug
+            #cv2.rectangle(img, (x,y), (x1,y1), (0,255,0))
+            #cv2.imwrite("save/" + str(x) +' _' + str(y) + ".png", tiles)
+            #cv2.imwrite("test.png", img)
     return leftsides, rightsides
 
-#    outFile = open('./out.txt', 'w+')
 
+#Makes string into an array of floats
 def processString(s):
    splits = s.splitlines()
-#   group = splits[0][0:6]
    nums = []
-   for line in splits[1:]:
+   for line in splits[1:]: #starts at line 1 to avoid group
         nums = nums + [float(re.sub('[^0-9.]', '', x)) for x in line.split()]
    return nums
-#   return [Group(group, nums)]
 
-def printToText(groupls, grouprs, outFile):
-    workbook = xlsxwriter.Workbook('out.xlsx')
+#Prints data to excel
+def printToText(dataLS, dataRS, outFile):
+    workbook = xlsxwriter.Workbook(outFile)
     worksheet = workbook.add_worksheet()
     row = 0
-    for col, arrays in enumerate(groupls):
+    for col, arrays in enumerate(dataLS):
         worksheet.write_column(row, col, arrays)
-    for col2, arrays2 in enumerate(grouprs):
-        worksheet.write_column(row, col2, arrays2)
+    for col, arrays in enumerate(dataRS):
+        worksheet.write_column(row, col + 4, arrays)
     workbook.close()
-#    az, bs, cs, ds = groupls
-#    z= map(None, az, bs, cs, ds)
 
-
-#    for x in groupls:
-#        print >> outFile, x.groupname + '\n'
-#        for datas in x.data:
-#            print >> outFile, datas
-
-
-pathname = "./test.bmp"
-image = cv2.imread(pathname)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-
-
-
-#image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-#image = cv2.bilateralFilter(image,9,75,75)
-#temp = cv2.GaussianBlur(image,(0,0),3)
-#temp = cv2.addWeighted(1.5, image, -0.5,0,image)
-
-image = cv2.equalizeHist(image)
+#MAIN
+pathname = "./pics/Number Grab example pic, 19, 04-04 1000 Pound, all black text.bmp"
+image = cv2.imread(pathname) 
 ls , rs =crop_Image(image)
-groupls = []
-grouprs = []
-for primes in ls:
-    tempstring = pytesseract.image_to_string(primes)
-    groupls = groupls +[processString(tempstring.encode("utf-8"))]
-for primes2 in rs:
-    tempstring = pytesseract.image_to_string(primes2)
-    groupsrs = grouprs + [processString(tempstring.encode("utf-8"))]
-printToText(groupls, grouprs, "txt.out")
-#outstring = pytesseract.image_to_string(image)
+dataLS = []
+dataRS = []
+for gPic in ls:
+    tempstring = pytesseract.image_to_string(gPic)
+    dataLS = dataLS + [processString(tempstring.encode("utf-8"))]
+for gPic in rs:
+    tempstring = pytesseract.image_to_string(gPic)
+    dataRS = dataRS + [processString(tempstring.encode("utf-8"))]
 
-#pytesseract('./test.jpg', 'output', lang=None, boxes=False, config="hocr")
-#print(dir(pytesseract))
-
-
-
-
-
-# print(pytesseract.image_to_boxes(Image.open(pathname)))
-
-
-
-    
+printToText(dataLS, dataRS, "out.xlsx") 
